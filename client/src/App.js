3ab251e907ac91473699548b4e8bac04bc4b1206
szczep4niak <jjdroid6969@gmail.com>
2019-12-14 21:44:26 +0100

    reformat

--- client/src/App.js
+++ client/src/App.js
@@ -3,91 +3,91 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Swapper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      leftWinOpen: true,
+    constructor(props) {
+        super(props);
+        this.state = {
+            leftWinOpen: true,
+        }
+        this.toggleLeft = this.toggleLeft.bind(this);
+        this.toggleRight = this.toggleRight.bind(this);
     }
-    this.toggleLeft = this.toggleLeft.bind(this);
-    this.toggleRight = this.toggleRight.bind(this);
-  }
 
-  toggleLeft() {
-    this.setState({ leftWinOpen: true});
-  }
+    toggleLeft() {
+        this.setState({ leftWinOpen: true});
+    }
 
-  toggleRight() {
-    this.setState({ leftWinOpen: false});
-  }
+    toggleRight() {
+        this.setState({ leftWinOpen: false});
+    }
 
-  render() {
-    const leftWinOpen = this.state.leftWinOpen;
-    return (
-      <div className="container card main-form">
-		<div className="card-header btn-group">
-        <button className="btn btn-secondar" onClick={this.toggleLeft}>Login</button>
-        <button className="btn btn-secondar" onClick={this.toggleRight}>Register</button>
-		</div>
-        {leftWinOpen ? (
-          <LoginForm />
-        ) : (
-          <RegisterForm />
-        )}
-      </div>
-    );
-  }
+    render() {
+        const leftWinOpen = this.state.leftWinOpen;
+        return (
+            <div className="container card main-form">
+            <div className="card-header btn-group">
+                <button className="btn btn-secondar" onClick={this.toggleLeft}>Login</button>
+                <button className="btn btn-secondar" onClick={this.toggleRight}>Register</button>
+            </div>
+            {leftWinOpen ? (
+                <LoginForm />
+            ) : (
+                <RegisterForm />
+            )}
+            </div>
+        );
+    }
 }
 
 
 class LoginForm extends React.Component {
-	render() {
-    return (
-        <form className="card-body">
-			<div className="form-group">
-				<label for="username">Username</label>  
-				<input type="text" id="username" class="form-control"></input>
-			</div>
-			<div className="form-group">
-				<label for="password">Password</label>
-				<input type="text" id="password" class="form-control"></input>
-			</div>
-         </form>
-    )
-	}
+    render() {
+        return (
+            <form className="card-body">
+            <div className="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" class="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label for="password">Password</label>
+                <input type="text" id="password" class="form-control"></input>
+            </div>
+            </form>
+        )
+    }
 }
 
 
 class RegisterForm extends React.Component {
-	render() {
-    return (
-        <form className="card-body">
-			<div className="form-group">
-				<label for="email">Email</label>
-				<input type="text" id="email" class="form-control"></input>
-			</div>		
-			<div className="form-group">
-				<label for="username">Username</label>  
-				<input type="text" id="username" class="form-control"></input>
-			</div>
-			<div className="form-group">
-				<label for="password">Password</label>
-				<input type="text" id="password" class="form-control"></input>
-			</div>
-         </form>
-    );
-	}
+    render() {
+        return (
+            <form className="card-body">
+            <div className="form-group">
+                <label for="email">Email</label>
+                <input type="text" id="email" class="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" class="form-control"></input>
+            </div>
+            <div className="form-group">
+                <label for="password">Password</label>
+                <input type="text" id="password" class="form-control"></input>
+            </div>
+            </form>
+        );
+    }
 }
 
 class App extends Component {
-  render() {
-	console.log('I was triggered during render');
-    return (
-        <div className="App">
-          <h1 className="jumbotron">SGPWCR</h1>
-          <Swapper />
-        </div>
-    );
-  }
+    render() {
+        console.log('I was triggered during render');
+        return (
+            <div className="App">
+            <h1 className="jumbotron">SGPWCR</h1>
+            <Swapper />
+            </div>
+        );
+    }
 }
 
 export default App;
